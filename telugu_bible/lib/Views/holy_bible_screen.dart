5b001b31d83c6ddc2331865e1d41134bc3bb10e0
darling <tiwariatul9526@gmail.com>
2023-06-28 17:43:38 +0530

    adding english translator

--- telugu_bible/lib/Views/holy_bible_screen.dart
+++ telugu_bible/lib/Views/holy_bible_screen.dart
@@ -5,6 +5,7 @@ import 'package:telugu_bible/services/firebase_services.dart';
 import 'package:telugu_bible/utis/app_style.dart';
 import 'package:flutter/services.dart';
 import 'package:share/share.dart';
+import 'package:translator/translator.dart';
 
 class HolyBibleScreen extends StatefulWidget {
   const HolyBibleScreen({Key? key}) : super(key: key);
@@ -19,6 +20,7 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
   late String selectedChapterName = '';
   late List<String> selectedChapterDataList = [];
   late String headingName = "";
+  final translator = GoogleTranslator();
 
   @override
   void initState() {
@@ -63,7 +65,7 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
     });
   }
 
-  void showCopyShareDialog(String data) {
+  void showCopyShareTranslateDialog(String data) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -86,6 +88,13 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
                   shareData(data);
                 },
               ),
+              IconButton(
+                icon: const Icon(Icons.translate),
+                onPressed: () {
+                  Navigator.pop(context);
+                  translateToEnglish(data);
+                },
+              ),
             ],
           ),
         );
@@ -102,6 +111,28 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
 
   void shareData(String data) {
     Share.share(data);
+  }
+
+  Future<void> translateToEnglish(String data) async {
+    final translation = await translator.translate(data, from: 'te', to: 'en');
+    // ignore: use_build_context_synchronously
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Translated word'),
+          content: Text(translation.text),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -185,7 +216,7 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
                             EdgeInsets.all(AppDimensionHelper.getHt(10)),
                         title: GestureDetector(
                           onTap: () {
-                            showCopyShareDialog(data);
+                            showCopyShareTranslateDialog(data);
                           },
                           child: Text(
                             data,
@@ -227,6 +258,7 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
 // import 'package:telugu_bible/services/firebase_services.dart';
 // import 'package:telugu_bible/utis/app_style.dart';
 // import 'package:flutter/services.dart';
+// import 'package:share/share.dart';
 
 // class HolyBibleScreen extends StatefulWidget {
 //   const HolyBibleScreen({Key? key}) : super(key: key);
@@ -285,11 +317,45 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
 //     });
 //   }
 
+//   void showCopyShareDialog(String data) {
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: const Text('Options'),
+//           content: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               IconButton(
+//                 icon: const Icon(Icons.copy),
+//                 onPressed: () {
+//                   Navigator.pop(context);
+//                   copyDataToClipboard(data);
+//                 },
+//               ),
+//               IconButton(
+//                 icon: const Icon(Icons.share),
+//                 onPressed: () {
+//                   Navigator.pop(context);
+//                   shareData(data);
+//                 },
+//               ),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+//   }
+
 //   void copyDataToClipboard(String data) {
 //     Clipboard.setData(ClipboardData(text: data));
 //     ScaffoldMessenger.of(context).showSnackBar(
 //       const SnackBar(content: Text('Data copied to clipboard')),
 //     );
+//   }
+
+//   void shareData(String data) {
+//     Share.share(data);
 //   }
 
 //   @override
@@ -373,7 +439,7 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
 //                             EdgeInsets.all(AppDimensionHelper.getHt(10)),
 //                         title: GestureDetector(
 //                           onTap: () {
-//                             copyDataToClipboard(data);
+//                             showCopyShareDialog(data);
 //                           },
 //                           child: Text(
 //                             data,
@@ -405,329 +471,6 @@ class _HolyBibleScreenState extends State<HolyBibleScreen> {
 //     );
 //   }
 // }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:lottie/lottie.dart';
-// import 'package:telugu_bible/helper/dimension_helper.dart';
-// import 'package:telugu_bible/services/firebase_services.dart';
-// import 'package:telugu_bible/utis/app_style.dart';
-
-// class HolyBibleScreen extends StatefulWidget {
-//   const HolyBibleScreen({Key? key}) : super(key: key);
-
-//   @override
-//   State<HolyBibleScreen> createState() => _HolyBibleScreenState();
-// }
-
-// class _HolyBibleScreenState extends State<HolyBibleScreen> {
-//   final firebaseServices = FirebaseServices();
-//   int currentChapterIndex = 0;
-//   late String selectedChapterName = '';
-//   late List<String> selectedChapterDataList = [];
-//   late String headingName = "";
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     loadData();
-//   }
-
-//   Future<void> loadData() async {
-//     final snapshot = await firebaseServices.holyBible
-//         .orderBy("created_at", descending: false)
-//         .get();
-
-//     if (snapshot.docs.isNotEmpty) {
-//       final holyData = snapshot.docs[currentChapterIndex];
-//       setState(() {
-//         headingName = holyData["heading"];
-//         selectedChapterName = holyData["chapterName"] ?? '';
-//         selectedChapterDataList = List<String>.from(holyData['dataList']);
-//       });
-//     }
-//   }
-
-//   void updateChapter(int index) {
-//     final snapshot = firebaseServices.holyBible
-//         .orderBy("created_at", descending: false)
-//         .snapshots()
-//         .asBroadcastStream();
-
-//     snapshot.listen((querySnapshot) {
-//       final holyData = querySnapshot.docs[index];
-//       setState(() {
-//         currentChapterIndex = index;
-//         selectedChapterName = holyData["chapterName"] ?? '';
-//         selectedChapterDataList = List<String>.from(holyData['dataList']);
-//       });
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         automaticallyImplyLeading: false,
-//         title: const Text("Holy Bible"),
-//         elevation: 0,
-//       ),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             if (selectedChapterName.isNotEmpty &&
-//                 selectedChapterDataList.isNotEmpty)
-//               Column(
-//                 children: [
-//                   SizedBox(height: AppDimensionHelper.getHt(10)),
-//                   ListTile(
-//                     title: Center(
-//                       child: Text(
-//                         headingName,
-//                         style: AppFontStyles.MediumHeadingText.copyWith(
-//                             fontSize: 25),
-//                       ),
-//                     ),
-//                   ),
-//                   SizedBox(height: AppDimensionHelper.getHt(20)),
-//                   ListTile(
-//                     title: Row(
-//                       children: [
-//                         IconButton(
-//                           icon: const Icon(Icons.arrow_back_ios, size: 25),
-//                           onPressed: () {
-//                             if (currentChapterIndex > 0) {
-//                               updateChapter(currentChapterIndex - 1);
-//                             }
-//                           },
-//                         ),
-//                         Expanded(
-//                           child: GestureDetector(
-//                             onTap: () {
-//                               // Handle chapter tap if needed
-//                             },
-//                             child: Row(
-//                               mainAxisAlignment: MainAxisAlignment.center,
-//                               children: [
-//                                 Text(
-//                                   selectedChapterName,
-//                                   style: AppFontStyles.smallText
-//                                       .copyWith(fontSize: 25),
-//                                 ),
-//                               ],
-//                             ),
-//                           ),
-//                         ),
-//                         IconButton(
-//                           icon: const Icon(Icons.arrow_forward_ios, size: 25),
-//                           onPressed: () {
-//                             if (currentChapterIndex <
-//                                 selectedChapterDataList.length - 1) {
-//                               updateChapter(currentChapterIndex + 1);
-//                             }
-//                           },
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                   SizedBox(height: AppDimensionHelper.getHt(30)),
-//                   ListView.builder(
-//                     shrinkWrap: true,
-//                     scrollDirection: Axis.vertical,
-//                     physics: const NeverScrollableScrollPhysics(),
-//                     itemCount: selectedChapterDataList.length,
-//                     itemBuilder: (context, index) {
-//                       return ListTile(
-//                         contentPadding:
-//                             EdgeInsets.all(AppDimensionHelper.getHt(10)),
-//                         title: Text(
-//                           selectedChapterDataList[index],
-//                           textAlign: TextAlign.justify,
-//                           style: AppFontStyles.smallText.copyWith(
-//                             fontSize: 20,
-//                             color: Colors.black,
-//                             fontWeight: FontWeight.w400,
-//                           ),
-//                         ),
-//                       );
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             if (selectedChapterName.isEmpty || selectedChapterDataList.isEmpty)
-//               Center(
-//                 child: Lottie.asset("assets/loading.json", repeat: true),
-//               ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart';
-// import 'package:lottie/lottie.dart';
-// import 'package:telugu_bible/services/firebase_services.dart';
-
-// class HolyBibleScreen extends StatefulWidget {
-//   const HolyBibleScreen({Key? key}) : super(key: key);
-
-//   @override
-//   State<HolyBibleScreen> createState() => _HolyBibleScreenState();
-// }
-
-// class _HolyBibleScreenState extends State<HolyBibleScreen> {
-//   final firebaseServices = FirebaseServices();
-//   static int currentChapterIndex = 0;
-//   String selectedChapterName = '';
-//   List<String> selectedChapterDataList = [];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     currentChapterIndex = 0;
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         automaticallyImplyLeading: false,
-//         title: const Text("Holy Bible"),
-//         elevation: 0,
-//       ),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             StreamBuilder<QuerySnapshot>(
-//               stream: firebaseServices.holyBible
-//                   .orderBy("created_at", descending: false)
-//                   .snapshots(),
-//               builder: (context, snapshot) {
-//                 if (snapshot.connectionState == ConnectionState.waiting) {
-//                   return Center(
-//                     child: Lottie.asset("assets/loading.json", repeat: true),
-//                   );
-//                 }
-//                 if (snapshot.hasData) {
-//                   final List<QueryDocumentSnapshot> holyDocs =
-//                       snapshot.data!.docs;
-//                   if (holyDocs.isNotEmpty) {
-//                     final holyData = holyDocs[currentChapterIndex];
-//                     final chapterName = holyData["chapterName"];
-//                     final headingName = holyData["heading"];
-//                     final dataList = List<String>.from(holyData['dataList']);
-
-//                     return Column(
-//                       children: [
-//                         ListTile(
-//                           title: Center(
-//                             child: Text(
-//                               headingName,
-//                               style: TextStyle(
-//                                 fontSize: 20,
-//                                 fontWeight: FontWeight.bold,
-//                               ),
-//                             ),
-//                           ),
-//                         ),
-//                         ListTile(
-//                           title: Row(
-//                             children: [
-//                               IconButton(
-//                                 icon: Icon(Icons.arrow_left),
-//                                 onPressed: () {
-//                                   setState(() {
-//                                     if (currentChapterIndex > 0) {
-//                                       currentChapterIndex--;
-//                                       updateSelectedChapter(holyDocs);
-//                                     }
-//                                   });
-//                                 },
-//                               ),
-//                               Expanded(
-//                                 child: GestureDetector(
-//                                   onTap: () {
-//                                     setState(() {
-//                                       selectedChapterName = chapterName;
-//                                       selectedChapterDataList = dataList;
-//                                     });
-//                                   },
-//                                   child: Row(
-//                                     mainAxisAlignment: MainAxisAlignment.center,
-//                                     children: [
-//                                       Text(
-//                                         chapterName,
-//                                         style: TextStyle(
-//                                           fontSize: 18,
-//                                           fontWeight: FontWeight.bold,
-//                                         ),
-//                                       ),
-//                                     ],
-//                                   ),
-//                                 ),
-//                               ),
-//                               IconButton(
-//                                 icon: Icon(Icons.arrow_right),
-//                                 onPressed: () {
-//                                   setState(() {
-//                                     if (currentChapterIndex <
-//                                         holyDocs.length - 1) {
-//                                       currentChapterIndex++;
-//                                       updateSelectedChapter(holyDocs);
-//                                     }
-//                                   });
-//                                 },
-//                               ),
-//                             ],
-//                           ),
-//                         ),
-//                         if (selectedChapterName.isNotEmpty &&
-//                             selectedChapterDataList.isNotEmpty)
-//                           ListView.builder(
-//                             shrinkWrap: true,
-//                             scrollDirection: Axis.vertical,
-//                             physics: const NeverScrollableScrollPhysics(),
-//                             itemCount: selectedChapterDataList.length,
-//                             itemBuilder: (context, index) {
-//                               return ListTile(
-//                                 title: Text(
-//                                   selectedChapterDataList[index],
-//                                 ),
-//                               );
-//                             },
-//                           ),
-//                       ],
-//                     );
-//                   } else {
-//                     return const Text('No Data Found');
-//                   }
-//                 }
-//                 if (snapshot.hasError) {
-//                   return const Text('Error Data Not Found');
-//                 } else {
-//                   return const Text('Error Data Not Found');
-//                 }
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   void updateSelectedChapter(List<QueryDocumentSnapshot> holyDocs) {
-//     final holyData = holyDocs[currentChapterIndex];
-//     selectedChapterName = holyData["chapterName"];
-//     selectedChapterDataList = List<String>.from(holyData['dataList']);
-//   }
-// }
-
 
 
 
